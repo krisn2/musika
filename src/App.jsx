8504e6b95e7bd6b2d1c69a35428ab2a893141967
krisn2 <krishnasarone08@gmail.com>
2024-12-28 20:48:34 +0530

    Work on hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Hero from './component/Hero'
 
 const App = () => {
   return (
-   <>
+    <div className="relative inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
    <Hero/>
-   </>
+   </div>
   )
 }
 
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -3,11 +3,30 @@ import Search from "./Search";
 
 const Hero = () => {
   return (
-    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        <Search/>
-        <div className="flex flex-col items-center mt-20 gap-8 text-center">
-          <h1 className="text-6xl font-semibold text-blue-50 ">Welcome to Musika</h1>
+    <div className="relative">
+      <Search />
+      <div className="flex flex-col items-center mt-20 gap-8 text-center">
+        <h1 className="text-6xl font-semibold text-blue-50 ">
+          Welcome to Musika
+        </h1>
+      </div>
+      <div className="relative top-20 w-full bg-black bg-opacity-50 text-blue-50 rounded-xl h-full flex flex-col items-center">
+        <div className="flex flex-row gap-8 mt-20 mb-5">
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
         </div>
+        <div className="flex flex-row justify-center items-center gap-8 mt-20 mb-5">
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+          <div className="bg-purple-600 bg-opacity-50 w-40 h-60 rounded-xl"></div>
+        </div>
+      </div>
+      
     </div>
   );
 };
